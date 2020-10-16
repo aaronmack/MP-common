c97d00a1a4207ebefd010c8be5c674121edc9301
Aaron <lilong999000@gmail.com>
2020-10-16 16:00:10 +0800

    [update] mp-common-pkg version {0.0.3}

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mp-common-pkg", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Aaron Li",
     author_email="lilong999000@gmail.com",
     description="A common repository, for MP System",
